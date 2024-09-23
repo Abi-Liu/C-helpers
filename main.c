62c86cbf2888c8c9ec1bf268eeac405d6c9d2f2f
Abi Liu <abiliu018@gmail.com>
2024-09-23 16:11:07 -0700

    tests

---
+++ main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "binary_tree.h"
+
+int main() {
+  /*
+  *           original tree structure:
+  *       preorder: [5, 3, 2, 4, 7, 6, 8]
+  *                   5
+  *                 /   \
+  *                3    7
+  *               / \  / \
+  *              2  4  6  8 
+  */
+
+  int arr[7] = {5, 3, 7, 2, 4, 6, 8};
+  bst *tree = build_bst(arr, 7);
+  print_bst(tree->root);
+  printf("\n");
+
+
+ /*
+  *          post add tree structure:
+  *       preorder: [5, 3, 2, 1, 4, 7, 6, 8]
+  *                   5
+  *                 /   \
+  *                3    7
+  *               / \  / \
+  *              2  4  6  8 
+  *             /
+  *            1
+  */
+  add(tree, 1);
+  print_bst(tree->root);
+  printf("\n");
+
+
+  /*
+  *           post remove 1 tree structure:
+  *       preorder: [5, 3, 2, 4, 7, 6, 8]
+  *                   5
+  *                 /   \
+  *                3    7
+  *               / \  / \
+  *              2  4  6  8 
+  */
+  remove_node(tree, 1);
+  print_bst(tree->root);
+  printf("\n");
+
+  /*
+  *           post remove 1 tree structure:
+  *       preorder: [4, 3, 2, 7, 6, 8]
+  *                   4
+  *                 /   \
+  *                3    7
+  *               /    / \
+  *              2     6  8 
+  */
+  remove_node(tree, 5);
+  print_bst(tree->root);
+  printf("\n");
+}
